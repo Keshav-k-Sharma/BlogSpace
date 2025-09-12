e8c8e44f33fe180c7b3ef84d60743fe9366c77a0
keshav krishan sharma <reach.keshavks@gmail.com>
2025-09-12 18:36:56 +0530

    fix template paths in URL routing

--- blogspace/settings.py
+++ blogspace/settings.py
@@ -65,7 +65,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR/'blogs'/'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'blogs/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,13 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'blogs/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-STATIC_ROOT=BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
